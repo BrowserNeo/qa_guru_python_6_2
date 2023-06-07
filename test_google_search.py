dd5fd0d6ab93343b1be757842214de9cf4ecfd08
George BrowserNeo <disconnect>
2023-06-07 16:01:07 -0500

    add requirements

--- test_google_search.py
+++ test_google_search.py
@@ -3,6 +3,6 @@ from selene import be, have
 
 
 browser.open('https://google.com')
-browser.element('#L2AGLb').click() #принять куки
+# browser.element('#L2AGLb').click() #принять куки
 browser.element('[name="q"]').should(be.blank).type('yashake/selene').press_enter()
 browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
